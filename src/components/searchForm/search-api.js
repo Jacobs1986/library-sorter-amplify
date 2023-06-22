c682b99df4d6f1441694825e3dfc70143cbf71f9
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-22 17:10:26 -0600

    Update search-api.js
    Creating a simple function just to test this out

---
+++ src/components/searchForm/search-api.js
@@ -0,0 +1,6 @@
+function searchForBook(searchInfo) {
+    const test = `The search input is ${searchInfo.input} and the serach type is ${searchInfo.type}`
+    return test
+}
+
+export default searchForBook
