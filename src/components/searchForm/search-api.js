27a09e10bbf958a53c30d1da4491f088a2048498
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-22 17:24:15 -0600

    Update search-api.js
    Testing out the switch function

--- src/components/searchForm/search-api.js
+++ src/components/searchForm/search-api.js
@@ -1,6 +1,18 @@
+var alert;
+
 function searchForBook(searchInfo) {
-    const test = `The search input is ${searchInfo.input} and the serach type is ${searchInfo.type}`
-    return test
+    switch(searchInfo.type) {
+        case "intitle": {
+            alert = "The title will be searched for";
+            return alert;
+        }
+        case "inauthor": {
+            alert = "The author will be searched for";
+            return alert;
+        }
+        default:
+            return
+    }
 }
 
 export default searchForBook
