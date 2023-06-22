8b789c165398341f9d4a527fe278c351b44d7715
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-22 13:50:30 -0600

    Imported form into searchForm

---
+++ src/components/searchForm/sarchForm.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// CSS File
+import "./searchForm.css";
+
+export default function searchForm() {
+    return (
+        <div>
+            <form>
+                {/* Type of search */}
+                <label for="search">Search by:</label>
+                <select id="search" name="search">
+                    <option>Title</option>
+                    <option>Author</option>
+                    <option>Subject</option>
+                    <option>ISBN</option>
+                </select>
+                {/* Input search information */}
+                <input 
+                    type="text"
+                    placeholder="Enter search here"
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
+    );
+};
--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -4,22 +4,6 @@ export default function Search() {
     return (
         <div>
             <h1>Search for a book</h1>
-            <form>
-                {/* Type of search */}
-                <label for="search">Search by:</label>
-                <select id="search" name="search">
-                    <option>Title</option>
-                    <option>Author</option>
-                    <option>Subject</option>
-                    <option>ISBN</option>
-                </select>
-                {/* Input search information */}
-                <input 
-                    type="text"
-                    placeholder="Enter search here"
-                />
-                <button type="submit">Search</button>
-            </form>
         </div>
     );
 };
