ea9ec5be000616878fc0a1652c1830140b30178e
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-22 13:45:55 -0600

    Update search-page.js
    Created a search form

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -2,6 +2,24 @@ import React from "react";
 
 export default function Search() {
     return (
-        <div>This is the search page!</div>
+        <div>
+            <h1>Search for a book</h1>
+            <form>
+                {/* Type of search */}
+                <label for="search">Search by:</label>
+                <select id="search" name="search">
+                    <option>Title</option>
+                    <option>Author</option>
+                    <option>Subject</option>
+                    <option>ISBN</option>
+                </select>
+                {/* Input search information */}
+                <input 
+                    type="text"
+                    placeholder="Enter search here"
+                />
+                <button type="submit">Search</button>
+            </form>
+        </div>
     );
 };
