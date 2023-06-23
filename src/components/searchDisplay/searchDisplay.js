0cc03305b628e2116ea93077f709bba7a314be2f
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 11:23:19 -0600

    Update searchDisplay.js

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -29,25 +29,40 @@ export default function SearchDisplay() {
                     There are no results
                 </div> :
                 <div>
-                    {/* Title of book */}
-                    <h2>{resultArray[0].volumeInfo.title}: {resultArray[0].volumeInfo.subtitle}</h2>
-                    {/* Bookcover */}
-                    <img
-                        src={resultArray[0].volumeInfo.imageLinks.thumbnail}
-                        alt="Book cover"
-                    />
-                    {/* Author */}
-                    <p>Author: {resultArray[0].volumeInfo.authors[0]}</p>
-                    {/* Publisher */}
-                    <p>Publisher: {resultArray[0].volumeInfo.publisher}</p>
-                    {/* Published Date */}
-                    <p>Published Date: {resultArray[0].volumeInfo.publishedDate}</p>
-                    {/* ISBN */}
-                    <p>ISBN: {resultArray[0].volumeInfo.industryIdentifiers[0].identifier}</p>
-                    {/* Description */}
-                    <h4>Description</h4>
-                    <p>{resultArray[0].volumeInfo.description}</p>
+                    {resultArray.map(book => (
+                        <div key={book.id}>
+                            {/* Book Title */}
+                            <h2>{book.volumeInfo.title}</h2>
+                            {/* Bookcover */}
+                            {!book.volumeInfo.imageLinks.thumbnail ?
+                                <div>No cover</div> :
+                                <img
+                                    src={book.volumeInfo.imageLinks.thumbnail}
+                                    alt="Book Cover"
+                                />}
+                        </div>
+                    ))}
                 </div>
+                // <div>
+                //     {/* Title of book
+                //     <h2>{resultArray[0].volumeInfo.title}: {resultArray[0].volumeInfo.subtitle}</h2>
+                //     {/* Bookcover */}
+                //     <img
+                //         src={resultArray[0].volumeInfo.imageLinks.thumbnail}
+                //         alt="Book cover"
+                //     />
+                //     {/* Author */}
+                //     <p>Author: {resultArray[0].volumeInfo.authors[0]}</p>
+                //     {/* Publisher */}
+                //     <p>Publisher: {resultArray[0].volumeInfo.publisher}</p>
+                //     {/* Published Date */}
+                //     <p>Published Date: {resultArray[0].volumeInfo.publishedDate}</p>
+                //     {/* ISBN */}
+                //     <p>ISBN: {resultArray[0].volumeInfo.industryIdentifiers[0].identifier}</p>
+                //     {/* Description */}
+                //     <h4>Description</h4>
+                //     <p>{resultArray[0].volumeInfo.description}</p> */}
+                // </div>
             }
         </div>
     );
