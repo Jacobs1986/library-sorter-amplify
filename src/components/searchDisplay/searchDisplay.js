ad608d7ad4da4e67c01e18dfab08c2b6e8718b41
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 09:44:58 -0600

    Update searchDisplay.js
    Imported ResultContext

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -3,6 +3,9 @@ import React, { useContext, useEffect } from "react";
 // CSS File
 import "./searchDisplay.css";
 
+// Import ResultContext
+import { ResultContext } from "../../pages/search-page";
+
 export default function SearchDisplay() {
     return (
         <div>The search display will go here.</div>
