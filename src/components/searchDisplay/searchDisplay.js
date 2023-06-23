783c1bfff625d2194df75575b37d7c28e4b89758
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 09:54:33 -0600

    Update searchDisplay.js
    Made a simple if/then statement just to check to make sure everything was working

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -9,6 +9,15 @@ import { ResultContext } from "../../pages/search-page";
 export default function SearchDisplay() {
     // Bring in result from ResultContext
     const { result } = useContext(ResultContext);
+
+    useEffect(() => {
+        if (result) {
+            console.log('A search was made')
+        } else {
+            console.log('No search has been performed')
+        }
+    }, [result])
+    
     return (
         <div>The search display will go here.</div>
     );
