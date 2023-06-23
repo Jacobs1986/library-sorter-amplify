7ee90137391aed62f28d5aa436f5f9f42dde9b7e
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 09:28:55 -0600

    Imported searchDisplay into the search-page

---
+++ src/components/searchDisplay/searchDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./searchDisplay.css";
+
+export default function SearchDisplay() {
+    return (
+        <div>The search display will go here.</div>
+    );
+};
--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -2,12 +2,14 @@ import React from "react";
 
 // Components
 import SearchForm from "../components/searchForm/searchForm";
+import SearchDisplay from "../components/searchDisplay/searchDisplay";
 
 export default function Search() {
     return (
         <div>
             <h1>Search for a book</h1>
             <SearchForm />
+            <SearchDisplay />
         </div>
     );
 };
