316e191427e80f348f4cf29a0e53a21593abc5d1
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 09:46:20 -0600

    Update searchDisplay.js
    Brought in result value from ResultContext

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -7,6 +7,8 @@ import "./searchDisplay.css";
 import { ResultContext } from "../../pages/search-page";
 
 export default function SearchDisplay() {
+    // Bring in result from ResultContext
+    const { result } = useContext(ResultContext);
     return (
         <div>The search display will go here.</div>
     );
