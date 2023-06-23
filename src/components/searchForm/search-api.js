e55b74df8f8fb57b93323fe2fb3d1ac2a9c6f5e6
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-22 18:18:17 -0600

    Update search-api.js
    Went back to doing a simple function

--- src/components/searchForm/search-api.js
+++ src/components/searchForm/search-api.js
@@ -1,13 +1,39 @@
+// Import axios
+// import axios from "axios";
+
 var alert = "Search type not recognized";
+// // Variables
+// var input;
+// var type;
+// var search;
+// var result;
+
+// const baseURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 function searchForBook(searchInfo) {
     switch(searchInfo.type) {
+        // Title Search
         case "intitle": {
             alert = "The title will be searched for";
             return alert;
         }
+        // Author search
         case "inauthor": {
             alert = "The author will be searched for";
+            return alert;
+        }
+        // ISBN search
+        case "isbn": {
+            alert = "The ISBN will be searched for";
+            // set type and input variables
+            // input = searchInfo.input;
+            // type = searchInfo.type;
+            // // set search variable
+            // search = `${baseURL}${input}+${type}`;
+            // // search for the info
+            // axios.get(search).then((response) => {
+            //     return response.data
+            // })
             return alert;
         }
         default:
