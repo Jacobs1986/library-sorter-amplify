647b08993b3c782cb2742360469d4188536813da
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 08:39:42 -0600

    Update searchForm.js
    Changed searchType to searchTerm

--- src/components/searchForm/searchForm.js
+++ src/components/searchForm/searchForm.js
@@ -10,7 +10,7 @@ import googleSearch from "./searchRefiner";
 
 export default function SearchForm() {
     // Hook for the search type
-    const [ searchType, setSearchType ] = useState('intitle');
+    const [ searchTerm, setSearchTerm ] = useState('intitle');
     // Hook for search input
     const [ searchInput, setSearchInput ] = useState('');
     // Hook for results
@@ -27,7 +27,7 @@ export default function SearchForm() {
         // Prevent the default
         event.preventDefault();
         // Setup the searchInfo variable
-        const searchInfo = { input: searchInput, type: searchType}
+        const searchInfo = { input: searchInput, terms: searchTerm}
         // Pass info to function and set variable
         const searchParam = googleSearch(searchInfo);
         console.log(searchParam);
@@ -41,8 +41,8 @@ export default function SearchForm() {
                 <select 
                     id="search" 
                     name="search" 
-                    onChange={event => setSearchType(event.target.value)}
-                    value={searchType}>
+                    onChange={event => setSearchTerm(event.target.value)}
+                    value={searchTerm}>
                     <option value="intitle">Title</option>
                     <option value="inauthor">Author</option>
                     <option value="subject">Subject</option>
