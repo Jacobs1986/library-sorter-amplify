4b84ae735b10dd165ec0462e489ddb6ad36d616d
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 19:44:59 -0600

    Update searchDisplay.js
    display is working

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -16,19 +16,35 @@ import axios from "axios";
 export default function SearchDisplay() {
     const { search } = useContext(SearchContext);
     // Make result hook for API
-    const [ result, setResult ] = useState([]);
+    const [result, setResult] = useState();
 
     useEffect(() => {
         if (search) {
             // Do API search
             axios.get(search).then((response) => {
-                setResult(response.data.items);
+                setResult(response.data);
             })
         }
     }, [search])
     return (
         <div>
-            Search results go here.
+            {/* Display the result */}
+            {!result ?
+                <div></div> :
+                <div>
+                    {result.items.map(book => (
+                        <div key={book.id}>
+                            {/* Book Title */}
+                            <h2>{book.volumeInfo.title}</h2>
+                            {/* Book Cover */}
+                            <img 
+                                src={book.volumeInfo.imageLinks.thumbnail}
+                                alt="Book Cover"
+                            />
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
     );
 };
