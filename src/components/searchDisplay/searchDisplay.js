2f12549eb694761801d10fb566aa6ba0f94af8f4
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-23 18:41:50 -0600

    Update searchDisplay.js
    Imported axios and did the api call

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -10,12 +10,20 @@ import "./searchDisplay.css";
 // Import ResultContext
 import { SearchContext } from "../../pages/search-page";
 
+// Import axios
+import axios from "axios";
+
 export default function SearchDisplay() {
     const { search } = useContext(SearchContext);
+    // Make result hook for API
+    const [ result, setResult ] = useState([]);
 
     useEffect(() => {
         if (search) {
-            console.log(search);
+            // Do API search
+            axios.get(search).then((response) => {
+                console.log(response.data.items);
+            })
         }
     }, [search])
     return (
