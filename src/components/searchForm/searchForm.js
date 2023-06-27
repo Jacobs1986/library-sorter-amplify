ec3d254a74f81469f9eb6c8f9927400ac4d1a1f7
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-27 15:08:42 -0600

    Update searchForm.js
    Added the form inputs into rows and columns

--- src/components/searchForm/searchForm.js
+++ src/components/searchForm/searchForm.js
@@ -11,9 +11,9 @@ import googleSearch from "./searchRefiner";
 
 export default function SearchForm() {
     // Hook for the search type
-    const [ searchTerm, setSearchTerm ] = useState('intitle');
+    const [searchTerm, setSearchTerm] = useState('intitle');
     // Hook for search input
-    const [ searchInput, setSearchInput ] = useState('');
+    const [searchInput, setSearchInput] = useState('');
     // Import context
     const { setSearch } = useContext(SearchContext);
 
@@ -22,7 +22,7 @@ export default function SearchForm() {
         // Prevent the default
         event.preventDefault();
         // Setup the searchInfo variable
-        const searchInfo = { input: searchInput, terms: searchTerm}
+        const searchInfo = { input: searchInput, terms: searchTerm }
         // Pass info to function and set variable
         setSearch(googleSearch(searchInfo));
         setSearchInput('');
@@ -32,24 +32,36 @@ export default function SearchForm() {
         <div className="container">
             <form onSubmit={handleSubmit}>
                 {/* Type of search */}
-                <label htmlFor="search">Search by:</label>
-                <select 
-                    id="search" 
-                    name="search" 
-                    onChange={event => setSearchTerm(event.target.value)}
-                    value={searchTerm}>
-                    <option value="intitle">Title</option>
-                    <option value="inauthor">Author</option>
-                    <option value="isbn">ISBN</option>
-                </select>
-                {/* Input search information */}
-                <input 
-                    type="text"
-                    placeholder="Enter search here"
-                    onChange={event => setSearchInput(event.target.value)}
-                    value={searchInput}
-                />
-                <input type="submit" value="Searcn" />
+                <div className="row">
+                    <div className="col-3">
+                        <label htmlFor="search">Search by:</label>
+                        <select
+                            id="search"
+                            name="search"
+                            onChange={event => setSearchTerm(event.target.value)}
+                            value={searchTerm}>
+                            <option value="intitle">Title</option>
+                            <option value="inauthor">Author</option>
+                            <option value="isbn">ISBN</option>
+                        </select>
+                    </div>
+                    {/* Input search information */}
+                    <div className="col-6">
+                        <label htmlFor="keywords">Search For:</label>
+                        <input
+                            id="keywords"
+                            type="text"
+                            placeholder="Enter search here"
+                            onChange={event => setSearchInput(event.target.value)}
+                            value={searchInput}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="col-12">
+                        <input type="submit" value="Search" />
+                    </div>
+                </div>
             </form>
         </div>
     );
