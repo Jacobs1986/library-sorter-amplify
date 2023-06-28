b4ec2058e5138ae8d1bda5729a9922413e4775be
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 14:17:27 -0600

    Update displayModal.js
    Made some basic code just to test out the modal

--- src/components/displayModal/displayModal.js
+++ src/components/displayModal/displayModal.js
@@ -5,6 +5,11 @@ import "./displayModal.css";
 
 export default function DisplayModal() {
     return (
-        <div>This is the display modal</div>
+        <div id="myModal" className="modal">
+            <div className="modal-content" >
+                <span className="close">&times;</span>
+                <p>Some text in the Modal...</p>
+            </div>
+        </div>
     );
 };
