cd1ee0bc160528207c0baa4156b2ea9475ecd94f
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 10:01:14 -0600

    Update searchDisplay.js
    Created a development variable

--- src/components/searchDisplay/searchDisplay.js
+++ src/components/searchDisplay/searchDisplay.js
@@ -19,6 +19,8 @@ export default function SearchDisplay() {
     const [result, setResult] = useState();
     // Creating a display hook 
     const [display, setDisplay] = useState();
+    // Variable that allows for development
+    const development = true;
 
     useEffect(() => {
         if (search) {
@@ -39,41 +41,47 @@ export default function SearchDisplay() {
             setResult('');
         }
     }, [result])
-
-    return (
-        <div className="container" id="displayContainer">
-            {!display ? null :
-                // Map the results
-                <div className="row">
-                    {/* If there are no results */}
-                    {display.totalItems === 0 ? <div>No results</div> :
-                        <div>
-                            {display.items.map((book, i) => (
-                                <div key={i} className="col-2">
-                                    {/* Check if imageLinks is undefined */}
-                                    {book.volumeInfo.imageLinks !== undefined
-                                    // If imageLinks in not undefined
-                                        ? <img
-                                            src={book.volumeInfo.imageLinks.thumbnail}
-                                            alt="Book Cover"
-                                        />
-                                        :
-                                        // If imageLinks is undefined 
-                                        <>
-                                            {/* Book Title */}
-                                            <h2>{book.volumeInfo.title}</h2>
-                                            {/* Cover image */}
-                                            <img 
-                                                src={require("./blank-cover.jpeg")}
-                                                alt="Book cover"
+    
+    if (development) {
+        return (
+            <div>Development mode is on</div>
+        )
+    } else {
+        return (
+            <div className="container" id="displayContainer">
+                {!display ? null :
+                    // Map the results
+                    <div className="row">
+                        {/* If there are no results */}
+                        {display.totalItems === 0 ? <div>No results</div> :
+                            <div>
+                                {display.items.map((book, i) => (
+                                    <div key={i} className="col-2">
+                                        {/* Check if imageLinks is undefined */}
+                                        {book.volumeInfo.imageLinks !== undefined
+                                            // If imageLinks in not undefined
+                                            ? <img
+                                                src={book.volumeInfo.imageLinks.thumbnail}
+                                                alt="Book Cover"
                                             />
-                                        </>
-                                    }
-                                </div>
-                            ))}
-                        </div>}
-                </div>
-            }
-        </div>
-    );
+                                            :
+                                            // If imageLinks is undefined 
+                                            <>
+                                                {/* Book Title */}
+                                                <h2>{book.volumeInfo.title}</h2>
+                                                {/* Cover image */}
+                                                <img
+                                                    src={require("./blank-cover.jpeg")}
+                                                    alt="Book cover"
+                                                />
+                                            </>
+                                        }
+                                    </div>
+                                ))}
+                            </div>}
+                    </div>
+                }
+            </div>
+        );
+    }
 };
