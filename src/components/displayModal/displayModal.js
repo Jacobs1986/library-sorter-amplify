6a8c8983f12695890eba31b32e775b251d9979ac
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 15:25:26 -0600

    Update displayModal.js
    Expanded the modal content

--- src/components/displayModal/displayModal.js
+++ src/components/displayModal/displayModal.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import "./displayModal.css";
 
 export default function DisplayModal() {
-    const [ showModal, setShowModal ] = useState("none")
+    const [ showModal, setShowModal ] = useState("block")
 
     // Create the function for opening the modal
     const handleOpenModal = () => {
@@ -21,8 +21,14 @@ export default function DisplayModal() {
             <button onClick={handleOpenModal}>Open modal</button>
             <div className="modal" style={{ display: `${showModal}`}}>
                 <div className="modal-content">
-                    <span className="close" onClick={handleCloseModal}>&times;</span>
-                    <p>Some text in the Modal...</p>
+                    <div className="modal-header">
+                        <span className="close" onClick={handleCloseModal}>&times;</span>
+                        <h2>Book Title</h2>
+                    </div>
+                    <div className="modal-body">
+                        <p>Some text in the Modal body</p>
+                        <p>Some other text...</p>
+                    </div>
                 </div>
             </div>
         </>
