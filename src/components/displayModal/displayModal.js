4b758caf356d5d9214f774c1f4e436b38361f3c7
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 15:41:44 -0600

    Update displayModal.js

--- src/components/displayModal/displayModal.js
+++ src/components/displayModal/displayModal.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import "./displayModal.css";
 
 export default function DisplayModal() {
-    const [showModal, setShowModal] = useState("block")
+    const [showModal, setShowModal] = useState("none")
 
     // Create the function for opening the modal
     const handleOpenModal = () => {
