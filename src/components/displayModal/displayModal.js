40716d2fac44bdda50c58bf37671dacc2fdd36fc
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 14:13:07 -0600

    Created the modal components

---
+++ src/components/displayModal/displayModal.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./displayModal.css";
+
+export default function DisplayModal() {
+    return (
+        <div>This is the display modal</div>
+    );
+};
