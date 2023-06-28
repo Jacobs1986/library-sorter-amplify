e8df75811ee8ebd36be69f6497d0f9f3548187c3
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-28 14:32:42 -0600

    Added styling and modal

--- src/components/displayModal/displayModal.js
+++ src/components/displayModal/displayModal.js
@@ -5,11 +5,15 @@ import "./displayModal.css";
 
 export default function DisplayModal() {
     return (
-        <div id="myModal" className="modal">
-            <div className="modal-content" >
-                <span className="close">&times;</span>
-                <p>Some text in the Modal...</p>
+        <>
+            <button>Open modal</button>
+            <div id="myModal" className="modal">
+                <div className="modal-content" >
+                    <span className="close">&times;</span>
+                    <p>Some text in the Modal...</p>
+                </div>
             </div>
-        </div>
+        </>
+
     );
 };
