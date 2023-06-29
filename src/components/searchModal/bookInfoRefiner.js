7a3f3ff91fb7231d8516eb4c71826ae89482368c
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-29 15:02:07 -0600

    Update bookInfoRefiner.js
    Writing some more to the refiner

--- src/components/searchModal/bookInfoRefiner.js
+++ src/components/searchModal/bookInfoRefiner.js
@@ -1,6 +1,9 @@
 // Function for refining the book information
 const bookInfoRefiner = (bookInfo) => {
-    const info = "The function is working";
+    // Set the title
+    const title = bookInfo.volumeInfo.title;
+    // Set the information to info
+    const info = {title: title}
     return info
 }
 
