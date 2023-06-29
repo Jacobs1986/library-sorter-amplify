9fe32e91887cbf203e1e3391667626664543a014
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-29 14:56:39 -0600

    Update bookInfoRefiner.js
    Put in some basic info for a test

---
+++ src/components/searchModal/bookInfoRefiner.js
@@ -0,0 +1,7 @@
+// Function for refining the book information
+const bookInfoRefiner = (bookInfo) => {
+    const info = "The function is working";
+    return info
+}
+
+export default bookInfoRefiner;
