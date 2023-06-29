7f5478060d190a4c59a55aaf8841ad65ad6caab8
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-29 13:38:54 -0600

    Update search-page.js
    Created hooks for googleId and also created a context GoogleBookId

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -7,15 +7,20 @@ import DisplayModal from "../components/searchModal/searchModal";
 
 // Create a context
 export const SearchContext = createContext();
+export const GoogleBookId = createContext();
 
 export default function Search() {
-    const [ search, setSearch ] = useState('');
+    const [search, setSearch] = useState('');
+    const [googleBookId, setGoogleBookId] = useState('');
+
     return (
         <SearchContext.Provider value={{ search, setSearch }}>
             <h1>Search for a book</h1>
             <SearchForm />
-            <SearchDisplay />
-            <DisplayModal />
+            <GoogleBookId.Provider value={{ googleBookId, setGoogleBookId }}>
+                <SearchDisplay />
+                <DisplayModal />
+            </GoogleBookId.Provider>
         </SearchContext.Provider>
     );
 };
