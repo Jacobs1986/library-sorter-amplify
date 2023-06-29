ea0d02bf94340164eb596678f4299144178b39d2
Sean Jacobson <sean.jacobson86@gmail.com>
2023-06-29 16:01:47 -0600

    Update bookInfoRefiner.js
    Wrote a switch statement for the images

--- src/components/searchModal/bookInfoRefiner.js
+++ src/components/searchModal/bookInfoRefiner.js
@@ -1,11 +1,22 @@
 // Function for refining the book information
 const bookInfoRefiner = (bookInfo) => {
+    let image;
     // Set the title
     const title = bookInfo.volumeInfo.title;
+    // Set the picture
+    // First see if imageLinks exists
+    switch(bookInfo.volumeInfo.imageLinks) {
+        case (!bookInfo.volumeInfo.imageLinks): {
+            image = "There are no images"
+            break
+        }
+        default:
+            image = "Image will be displayed"
+    }
     // Set the author(s)
     const author = bookInfo.volumeInfo.authors.join(", ")
     // Set the information to info
-    const info = {title: title, author: author}
+    const info = {title: title, author: author, image: image}
     return info
 }
 
