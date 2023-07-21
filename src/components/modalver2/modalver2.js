16398c326d5e5bc2d27b51544bd36c5ee125f313
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-21 14:49:28 -0600

    Created a second version of the modal

---
+++ src/components/modalver2/modalver2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./modalver2.css";
+
+export default function modalver2() {
+    return (
+        <div>This is the second version of the modal</div>
+    );
+};
