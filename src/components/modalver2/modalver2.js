127273b3879b38fd2cf7d582610efef4a919568d
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-21 15:18:55 -0600

    Created tabs

--- src/components/modalver2/modalver2.js
+++ src/components/modalver2/modalver2.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 // CSS File
 import "./modalver2.css";
 
-export default function modalver2() {
+export default function ModalVer2() {
+    // toggle modal state
+    const [ showModal, setShowModal ] = useState("block")
+
+     // Close the modal
+     const handleCloseModal = () => {
+        setShowModal("none");
+    }
+
     return (
-        <div>This is the second version of the modal</div>
+        <div className="modal" style={{ display: `${showModal}` }}>
+            <div className="modal-content">
+                <div className="modal-header">
+                    <span className="close" onClick={handleCloseModal}>&times;</span>
+                    <h2>Tabbed Modal</h2>
+                </div>
+                <div className="modal-body">
+                    <div className="modal-tabs">
+                        <button className="tab">Tab 1</button>
+                        <button className="tab">Tab 2</button>
+                    </div>
+                    <p>This is the first tab</p>
+                </div>
+            </div>
+        </div>
     );
 };
