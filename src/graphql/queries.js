49ff8551bc8047f9d84c5a9ffead200070760af7
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-21 10:54:37 -0600

    Created GraphQL database

---
+++ src/graphql/queries.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = /* GraphQL */ `
+  query GetNote($id: ID!) {
+    getNote(id: $id) {
+      id
+      cover
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listNotes = /* GraphQL */ `
+  query ListNotes(
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        cover
+        description
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
