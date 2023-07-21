f28d8d98ce62756bb23986566d8766f423353c98
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-21 16:29:43 -0600

    Imported the rest of the styling for information

--- src/components/modalver2/bookInfoComp.js
+++ src/components/modalver2/bookInfoComp.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// Import context
+import { SearchModalContext } from "./modalver2";
 
 export default function BookInfo() {
+    // set volume info
+    const { volumeInfo } = useContext(SearchModalContext);
+
     return (
-        <div>This is the book info component</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    {/* Display the image */}
+                    <img
+                        src={volumeInfo.cover}
+                        alt="Book cover"
+                        style={{ width: "100%" }}
+                    />
+                </div>
+                <div className="col-6 bookInfo">
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>Author</span>: {volumeInfo.author}</p>
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>Publisher</span>: {volumeInfo.publisher}</p>
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>Publication Date:</span>: {volumeInfo.publishDate}</p>
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>ISBN</span>: {volumeInfo.isbn.identifier}</p>
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>Page Count</span>: {volumeInfo.pageCount}</p>
+                    <p><span className="modalInlineLabel" style={{ fontWeight: "bold" }}>Description</span>:</p>
+                    <p id="descriptionParagraph">
+                        {volumeInfo.description}
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 };
