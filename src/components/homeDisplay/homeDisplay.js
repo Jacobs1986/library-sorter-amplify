338733faa2c5d75a27ce11a62911701a1712c0fb
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-21 09:39:02 -0600

    Moved displaycontainer to App.css; put in some test images and gave the first div of homeDisplay the className displayContainer

--- src/components/homeDisplay/homeDisplay.js
+++ src/components/homeDisplay/homeDisplay.js
@@ -5,6 +5,19 @@ import "./homeDisplay.css";
 
 export default function HomeDisplay() {
     return (
-        <div>This is the homeDisplay</div>
+        <div className="displayContainer">
+            <>
+                <img 
+                    src="http://books.google.com/books/publisher/content?id=6RSoAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71tO57SU0_YMc9WBnSum0SqmlCEqVhJxg28MP9HtOHDe1X7EM2IaAtGnXlvRwwP88y2gWqT7PymnNFUqMW6efAFSmpOF-eEdzY0j4Mu5PlTH6jIUxuEgrmmlo0jLb8Ey5oYfTHW&source=gbs_api"
+                    alt="Book cover"
+                />
+            </>
+            <>
+                <img 
+                    src="./Images/blank-cover.png"
+                    alt="Book cover"
+                />
+            </>
+        </div>
     );
 };
--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -6,6 +6,7 @@ import HomeDisplay from "../components/homeDisplay/homeDisplay";
 export default function Home() {
     return (
         <div>
+            <h1>Library</h1>
             <HomeDisplay />
         </div>
     );
