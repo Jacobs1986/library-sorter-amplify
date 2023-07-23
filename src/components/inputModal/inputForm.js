5d5a60bb4941f8b18a4d92012622aec9881dd687
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-23 17:14:32 -0600

    Imported context and created a handle function that will handle the text inputs

--- src/components/inputModal/inputForm.js
+++ src/components/inputModal/inputForm.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // CSS File
 import "./inputModal.css";
 
+// Import context
+import { NewBookInput } from "./inputModal";
+
 export default function InputForm() {
+    const { newBookInfo, setNewBookInfo } = useContext(NewBookInput);
+
+    // Function for handling change in the form inputs
+    const handleInputChange = event => {
+        setNewBookInfo({
+            name: event.target.name,
+            value: event.target.value
+        })
+    }
+
     return (
         <form>
             <div className="container inputBasicForm">
@@ -11,38 +24,38 @@ export default function InputForm() {
                 <div className="row">
                     <div className="col-12">
                         <label htmlFor="newTitle">Title</label>
-                        <input id="newTitle" type="text" name="newTitle" />
+                        <input id="newTitle" type="text" name="title" value={newBookInfo.title || ''} onChange={handleInputChange} />
                     </div>
                 </div>
                 <div className="row">
                     {/* Author */}
                     <div className="col-6">
                         <label htmlFor="newAuthor">Author(s)</label>
-                        <input id="newAuthor" type="text" name="newAuthor" />
+                        <input id="newAuthor" type="text" name="author" value={newBookInfo.author || ''} onChange={handleInputChange} />
                         <p style={{ margin: "0" }}>Use a comma to separate multiple authors</p>
                     </div>
                     {/* Publisher */}
                     <div className="col-6">
                         <label htmlFor="newPublisher">Publisher</label>
-                        <input id="newPublisher" type="text" name="newPublisher" />
+                        <input id="newPublisher" type="text" name="publisher" value={newBookInfo.publisher || ''} onChange={handleInputChange}/>
                     </div>
                 </div>
                 <div className="row">
                     {/* ISBN */}
                     <div className="col-4">
                         <label htmlFor="newISBN">ISBN</label>
-                        <input id="newISBN" type="text" name="newISBN" />
+                        <input id="newISBN" type="text" name="isbn" value={newBookInfo.isbn || ''} onChange={handleInputChange}/>
                     </div>
                     {/* Publication Date */}
                     <div className="col-4">
                         <label htmlFor="newPubDate">Publication Date</label>
-                        <input id="newPubDate" type="text" name="newPubDate" />
+                        <input id="newPubDate" type="text" name="pubDate" value={newBookInfo.pubDate || ''} onChange={handleInputChange}/>
                     </div>
                     {/* Number of pages */}
                     <div className="col-4">
                         <label htmlFor="newNumofPage">Number of Pages</label>
                         <br />
-                        <input id="newNumofPage" type="number" name="newNumofPage" />
+                        <input id="newNumofPage" type="number" name="numOfPages" value={newBookInfo.numOfPages || ''} onChange={handleInputChange}/>
                     </div>
                 </div>
                 <div className="row">
