dccb9e06a710aa068b626bb532fd2020a4f27b37
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-24 13:36:02 -0600

    Got everything setup so that information from the advanced form will be captured

--- src/components/inputModal/advancedForm.js
+++ src/components/inputModal/advancedForm.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // CSS File
 import "./inputModal.css";
 
+// Import context
+import { NewBookInput } from "./inputModal";
+
 export default function AdvancedForm() {
+    const { newBookInfo, setNewBookInfo } = useContext(NewBookInput);
+
+    // Function for handling change in the form inputs
+    const handleInputChange = event => {
+        setNewBookInfo({
+            name: event.target.name,
+            value: event.target.value
+        })
+    }
+
     return (
         <div>
             <h3>Advanced</h3>
@@ -16,52 +29,57 @@ export default function AdvancedForm() {
                             <div className="row">
                                 {/* Edition Number */}
                                 <label htmlFor="newEdition">Edition:</label>
-                                <input id="newEdition" type="number" name="newEdition" />
+                                <input id="newEdition" type="number" name="edition" value={ newBookInfo.edition || '' } onChange={handleInputChange} />
                             </div>
                             <div className="row">
                                 {/* Printing */}
                                 <label htmlFor="newPrinting">Printing:</label>
-                                <input id="newPrinting" type="number" name="newPrinting" />
+                                <input id="newPrinting" type="number" name="printing" value={ newBookInfo.printing || '' } onChange={handleInputChange}/>
                             </div>
                         </div>
                         <div className="col-4">
                             {/* Book Cover */}
                             <h3>Does the book have a dust jacket?</h3>
                             <div className="row radioRow">
-                                <input type="radio" id="newDustJacketYes" name="bookCoverQuestion" value="Yes" />
+                                <input type="radio" id="newDustJacketYes" name="dustJacket" value="Yes" onChange={handleInputChange} />
                                 <label htmlFor="newDustJacketYes">Yes</label>
-                                <input type="radio" id="newDustJacketNo" name="bookCoverQuestion" value="No" />
+                                <input type="radio" id="newDustJacketNo" name="dustJacket" value="No" onChange={handleInputChange} />
                                 <label htmlFor="newDustJacketNo">No</label>
                             </div>
                             {/* Book Cover condition */}
                             <div className="row">
                                 <label htmlFor="newBookCondition">Book Condition:</label>
-                                <select id="newBookCondition" name="newBookCondition">
-                                    <option value="excellent">Excellent</option>
-                                    <option value="veryGood">Very Good</option>
-                                    <option value="good">Good</option>
-                                    <option value="fair">Fair</option>
-                                    <option value="poor">Poor</option>
+                                <select id="newBookCondition" name="bookCondition" value={ newBookInfo.bookCondition || "" } onChange={handleInputChange} >
+                                    <option value="Excellent">Excellent</option>
+                                    <option value="Very Good">Very Good</option>
+                                    <option value="Good">Good</option>
+                                    <option value="Fair">Fair</option>
+                                    <option value="Poor">Poor</option>
                                 </select>
                             </div>
                             {/* Description of cover damage */}
                             <div className="row">
                                 <label htmlFor="newBookDamage">Description of damage:</label>
-                                <textarea id="newBookDamage" name="newBookDamage" rows="3" />
+                                <textarea id="newBookDamage" name="bookDamage" rows="3" value={ newBookInfo.bookDamage || '' } onChange={handleInputChange} />
                             </div>
                         </div>
                         <div className="col-4">
                             {/* Acquisition */}
                             <h3>Acquisition Information</h3>
+                            {/* Acquisition Date */}
+                            <div className="row">
+                                <label htmlFor="newAcquisitionDate">Acquisition Date:</label>
+                                <input type="date" id="newAcquisitionDate" name="acquisitionDate" value={ newBookInfo.acquisitionDate || '' } onChange={handleInputChange} />
+                            </div>
                             {/* Acquired from */}
                             <div className="row">
                                 <label htmlFor="newAcquiredFrom">Acquired From:</label>
-                                <input type="text" id="newAcquiredFrom" name="newAcquiredFrom" />
+                                <input type="text" id="newAcquiredFrom" name="acquiredFrom" value={ newBookInfo.acquiredFrom || '' } onChange={handleInputChange} />
                             </div>
                             {/* Cost */}
                             <div className="row">
                                 <label htmlFor="newAquiredCost">Cost</label>
-                                <input type="text" id="newAquiredCost" name="newAquiredCost" />
+                                <input type="text" id="newAquiredCost" name="aquiredCost" value={ newBookInfo.aquiredCost || '' } onChange={handleInputChange} />
                             </div>
                         </div>
                     </div>
