f870a6d8d25ee058c295f93e3bce7756b2d9a111
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-27 11:40:06 -0600

    Wrote a query that will only return the title, cover and database id; setup the book display; wrote a function to handle viewing a book

--- src/components/homeDisplay/homeDisplay.js
+++ src/components/homeDisplay/homeDisplay.js
@@ -1,23 +1,64 @@
-import React from "react";
+import React, {
+    useState,
+    useEffect
+} from "react";
+
+// Import API
+import { API } from "aws-amplify";
+
+// Import GraphQl query
+import { listBooksDisplay } from "../../graphql/queries";
 
 // CSS File
 import "./homeDisplay.css";
 
 export default function HomeDisplay() {
+    // Book list hook
+    const [bookList, setBookList] = useState();
+
+    useEffect(() => {
+        // Get all of the books from the database
+        API.graphql({
+            query: listBooksDisplay
+        }).then(res => {
+            setBookList(res.data.listBooks.items)
+        })
+    })
+
+    // Function for handling viewing a book
+    const handleViewBook = (event, bookId) => {
+        event.preventDefault();
+        console.log(bookId);
+    }
+
     return (
-        <div className="displayContainer">
-            <>
-                <img 
-                    src="http://books.google.com/books/publisher/content?id=6RSoAAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE71tO57SU0_YMc9WBnSum0SqmlCEqVhJxg28MP9HtOHDe1X7EM2IaAtGnXlvRwwP88y2gWqT7PymnNFUqMW6efAFSmpOF-eEdzY0j4Mu5PlTH6jIUxuEgrmmlo0jLb8Ey5oYfTHW&source=gbs_api"
-                    alt="Book cover"
-                />
-            </>
-            <>
-                <img 
-                    src="./Images/blank-cover.png"
-                    alt="Book cover"
-                />
-            </>
+        <div>
+            {!bookList ? <div>No Books</div> :
+                <div className="displayContainer homePageDisplay">
+                    {bookList.map(book => (
+                        <div className="coverContainer" key={book.id}>
+                            <img
+                                src={book.cover}
+                                alt="Book Cover"
+                                className="coverImage"
+                                style={{ width: "100%" }}
+                            />
+                            {book.cover === "./Images/blank-cover.png" ?
+                                <h4>{book.title}</h4> :
+                                <div></div>
+                            }
+                            <div className="middle">
+                                <div
+                                    className="coverButton"
+                                    value={book.id}
+                                    onClick={(event) => handleViewBook(event, book.id)}>
+                                    View More Info
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            }
         </div>
     );
 };
