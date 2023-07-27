a380086ab09a648f8a652fc03e3fa978ce7c2dfb
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-27 11:56:59 -0600

    Began building the display modal for book viewing

--- src/components/displayBookModal/displayBookModal.js
+++ src/components/displayBookModal/displayBookModal.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {
+    useState
+} from "react";
 
 // CSS File
 import "./displayBookModal.css";
 
 export default function DisplayBookModal() {
+    // Hook for showing displayModal
+    const [showDisplayModal, setShowDisplayModal] = useState("block");
+
+    // Function for closing the modal
+    const handleCloseDisplay = () => {
+        setShowDisplayModal("none")
+    }
+
     return (
-        <div>This is display modal for a specific book</div>
+        <div className="modal" style={{ display: `${showDisplayModal}` }}>
+            <div className="modal-content">
+                <div className="modal-header">
+                    <span className="close" onClick={handleCloseDisplay}>&times;</span>
+                    <h2>Book Title</h2>
+                </div>
+            </div>
+        </div>
     );
 };
