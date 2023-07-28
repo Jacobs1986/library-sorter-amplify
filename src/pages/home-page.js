b9d13d6e8e8d1ad2da66dc4d4eb3f36429eed8f6
Sean Jacobson <sean.jacobson86@gmail.com>
2023-07-28 14:06:57 -0600

    Added a search books link

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -6,7 +6,7 @@ import HomeDisplay from "../components/homeDisplay/homeDisplay";
 export default function Home() {
     return (
         <div>
-            <h1>Library</h1>
+            <h1>Library</h1> <a href="/search">Search for Books</a>
             <HomeDisplay />
         </div>
     );
