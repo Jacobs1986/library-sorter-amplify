7a61e34bbdf3f509b662090d084a387d718a7bd7
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-27 11:01:38 -0600

    Created a banner and text; also made a css file that will hold the media queries.

--- src/components/homePage/banner.js
+++ src/components/homePage/banner.js
@@ -2,9 +2,27 @@ import React from "react";
 
 // CSS File
 import "./homePage-styles.css";
+import "./homePage-styles-media.css";
 
 export default function HomePageBanner() {
     return (
-        <div>This is the banner.</div>
+        <div>
+            <div className="bannerContainer">
+                {/* Image */}
+                <img
+                    src="./Images/home-page-banner.jpg"
+                    alt="Picture of an antique bookshelf. Image provided by vecstock."
+                    width={"100%"}
+                />
+                {/* Name of site */}
+                <div className="centerBannerText">
+                    Library Database
+                </div>
+                {/* Attribution link */}
+                <div className="bottom-right attributionLink">
+                    Image By <a href="https://www.freepik.com/free-ai-image/abundant-collection-antique-books-wooden-shelves-generated-by-ai_42138866.htm#query=library%20background&position=0&from_view=keyword&track=ais">vecstock</a>
+                </div>
+            </div>
+        </div>
     );
 };
