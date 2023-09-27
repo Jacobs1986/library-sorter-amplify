24025e92f1f3057e3873066b2a87fbf33e8ddd87
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-27 14:00:19 -0600

    Rewrote the accordion function again; now only one library can be open at a time.

--- src/components/homePage/accordion.js
+++ src/components/homePage/accordion.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, {
+    useState
+} from "react";
 
 // CSS File
 import "./styles/accordion-styles.css";
 
 export default function AccordionView() {
+    // prevId
+    const [prevId, setPrevId] = useState("");
+
     const handleShowLib = event => {
         // First get the id of the element
         let elementId = event.target.id;
         // Get the next sibling
         let panel = document.getElementById(elementId).nextElementSibling;
-        console.log(panel.className);
-        // If panel.className === "panel" add a responsive className
-        if (panel.className === "panel") {
+         // Check to see if the prevId is unknown
+        // If it is defined then the previous panel needs to be closed
+        if (prevId !== "") {
+            // Set prevPanel variable
+            let prevPanel = document.getElementById(prevId).nextElementSibling;
+            // add responsive to panel.className
             panel.className += " responsive";
+            // Remove responsive from the previous panel
+            prevPanel.className = "panel";
+            // set elementId to prevId
+            setPrevId(elementId);
         } else {
-            panel.className = "panel";
+            // add responsive to the panel.className
+            panel.className += " responsive";
+            // set elementId to prevId
+            setPrevId(elementId);
         }
     }
-    // var acc = document.getElementsByClassName("accordion");
-    // var i;
-
-    // for (i = 0; i < acc.length; i++) {
-    //     acc[i].addEventListener("click", function () {
-    //         /* Toggle between adding and removing the "active" class,
-    //         to highlight the button that controls the panel */
-    //         this.classList.toggle("active");
-
-    //         /* Toggle between hiding and showing the active panel */
-    //         var panel = this.nextElementSibling;
-    //         if (panel.style.display === "block") {
-    //             panel.style.display = "none";
-    //         } else {
-    //             panel.style.display = "block";
-    //         }
-    //     });
-    // }
+    
     return (
         <div className="row">
             <div className="col-xs-12">
