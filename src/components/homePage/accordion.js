f588f5194bb91107417483b34390e3d449d73860
Sean Jacobson <sean.jacobson86@gmail.com>
2023-09-27 11:44:41 -0600

    Created a basic accordion

--- src/components/homePage/accordion.js
+++ src/components/homePage/accordion.js
@@ -1,12 +1,41 @@
 import React from "react";
 
 // CSS File
+import "./styles/accordion-styles.css";
 
 export default function AccordionView() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
     return (
         <div className="row">
             <div className="col-xs-12">
-                This is the accordion view.
+                {/* Accordion button */}
+                <button className="accordion">Library 1</button>
+                {/* Panel for the accordion */}
+                <div className="panel">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <button className="accordion">Library 2</button>
+                {/* Panel for the accordion */}
+                <div className="panel">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
             </div>
         </div>
     );
