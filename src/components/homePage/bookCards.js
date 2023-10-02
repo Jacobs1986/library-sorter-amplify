26461d652f944a6227a723d33e687d48f76ab33a
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-02 11:31:08 -0600

    Added a overflow: scroll to .panel.responsive; added some styling to the cards

--- src/components/homePage/bookCards.js
+++ src/components/homePage/bookCards.js
@@ -7,7 +7,21 @@ export default function BookCards() {
     return (
         <div>
             {/* The card div */}
-            <div className="card">
+            <div className="bookCard">
+                {/* Book image */}
+                <img 
+                    src="./Images/blank-cover.png"
+                    alt="A blank cover image"
+                    width={"100%"}
+                />
+                {/* Book info */}
+                <div className="bookCard-info">
+                    <h4><b>Title</b></h4>
+                    <p>Author's Name</p>
+                </div>
+            </div>
+            {/* The card div */}
+            <div className="bookCard">
                 {/* Book image */}
                 <img 
                     src="./Images/blank-cover.png"
