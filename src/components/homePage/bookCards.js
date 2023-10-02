7041757a50e15db16b074617c1c8c35547cf0ca2
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-02 15:33:53 -0600

    Finished making the map of the book list

--- src/components/homePage/bookCards.js
+++ src/components/homePage/bookCards.js
@@ -3,57 +3,31 @@ import React from "react";
 // CSS File
 import "./styles/bookCard-styles.css";
 
+// Import json file
+import bookList from "./testBooks.json";
+
 export default function BookCards() {
     return (
         <div className="row">
-            <div className="col-xs-12 col-s-4 col-xl-2">
-                {/* The card div */}
-                <div className="bookCard">
-                    {/* Book image */}
-                    <img
-                        src="./Images/blank-cover.png"
-                        alt="A blank cover image"
-                        width={"100%"}
-                    />
-                    {/* Book info */}
-                    <div className="bookCard-info">
-                        <h4><b>Title</b></h4>
-                        <p>Author's Name</p>
+            {/* Map the information */}
+            {bookList.map((book, i) => (
+                <div className="col-xs-12 col-s-4 col-xl-2" key={i}>
+                    {/* The card div */}
+                    <div className={book.cover === "blank-cover.png" ? "bookCard noCover" : "bookCard"}>
+                        {/* Book image */}
+                        <img 
+                            src={`./Images/${book.cover}`}
+                            alt="A picture of the book cover"
+                            width={"100%"}
+                        />
+                        {/* Book Info */}
+                        <div className="bookCard-info" style={book.cover === "blank-cover.png" ? { display: "block" } : { display: "none" }}>
+                            <h4><b>{book.title}</b></h4>
+                            <p>{book.author}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="col-xs-12 col-s-4 col-xl-2">
-                {/* The card div */}
-                <div className="bookCard">
-                    {/* Book image */}
-                    <img
-                        src="./Images/blank-cover.png"
-                        alt="A blank cover image"
-                        width={"100%"}
-                    />
-                    {/* Book info */}
-                    <div className="bookCard-info">
-                        <h4><b>Title</b></h4>
-                        <p>Author's Name</p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xs-12 col-s-4 col-xl-2">
-                {/* The card div */}
-                <div className="bookCard">
-                    {/* Book image */}
-                    <img
-                        src="./Images/blank-cover.png"
-                        alt="A blank cover image"
-                        width={"100%"}
-                    />
-                    {/* Book info */}
-                    <div className="bookCard-info">
-                        <h4><b>Title</b></h4>
-                        <p>Author's Name</p>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
     );
 };
