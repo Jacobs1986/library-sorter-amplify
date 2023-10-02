bb64ee6ed4bd7ef6bf6a394c094a4a15f163082d
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-02 11:39:08 -0600

    Added row and columns to the cards

--- src/components/homePage/bookCards.js
+++ src/components/homePage/bookCards.js
@@ -5,33 +5,53 @@ import "./styles/bookCard-styles.css";
 
 export default function BookCards() {
     return (
-        <div>
-            {/* The card div */}
-            <div className="bookCard">
-                {/* Book image */}
-                <img 
-                    src="./Images/blank-cover.png"
-                    alt="A blank cover image"
-                    width={"100%"}
-                />
-                {/* Book info */}
-                <div className="bookCard-info">
-                    <h4><b>Title</b></h4>
-                    <p>Author's Name</p>
+        <div className="row">
+            <div className="col-xs-12 col-s-4 col-xl-3">
+                {/* The card div */}
+                <div className="bookCard">
+                    {/* Book image */}
+                    <img
+                        src="./Images/blank-cover.png"
+                        alt="A blank cover image"
+                        width={"100%"}
+                    />
+                    {/* Book info */}
+                    <div className="bookCard-info">
+                        <h4><b>Title</b></h4>
+                        <p>Author's Name</p>
+                    </div>
                 </div>
             </div>
-            {/* The card div */}
-            <div className="bookCard">
-                {/* Book image */}
-                <img 
-                    src="./Images/blank-cover.png"
-                    alt="A blank cover image"
-                    width={"100%"}
-                />
-                {/* Book info */}
-                <div className="bookCard-info">
-                    <h4><b>Title</b></h4>
-                    <p>Author's Name</p>
+            <div className="col-xs-12 col-s-4 col-xl-3">
+                {/* The card div */}
+                <div className="bookCard">
+                    {/* Book image */}
+                    <img
+                        src="./Images/blank-cover.png"
+                        alt="A blank cover image"
+                        width={"100%"}
+                    />
+                    {/* Book info */}
+                    <div className="bookCard-info">
+                        <h4><b>Title</b></h4>
+                        <p>Author's Name</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col-xs-12 col-s-4 col-xl-3">
+                {/* The card div */}
+                <div className="bookCard">
+                    {/* Book image */}
+                    <img
+                        src="./Images/blank-cover.png"
+                        alt="A blank cover image"
+                        width={"100%"}
+                    />
+                    {/* Book info */}
+                    <div className="bookCard-info">
+                        <h4><b>Title</b></h4>
+                        <p>Author's Name</p>
+                    </div>
                 </div>
             </div>
         </div>
