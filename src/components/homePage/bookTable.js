4ead670cba98cbcf3eaaf0cfe2c2a04df5652907
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-03 11:12:47 -0600

    Created a basic table

--- src/components/homePage/bookTable.js
+++ src/components/homePage/bookTable.js
@@ -5,6 +5,24 @@ import "./styles/bookTable-styles.css";
 
 export default function BookTable() {
     return (
-        <div>The book table goes here.</div>
+        <div className="row">
+            {/* The table */}
+            <div className="col-xs-12" style={{ overflowX: "auto" }}>
+                <table className="bookTable">
+                    {/* Table header */}
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>ISBN</th>
+                    </tr>
+                    {/* Table row */}
+                    <tr>
+                        <td>Jung and Tarot: An Archetypal Journey</td>
+                        <td>Sallie Nichols</td>
+                        <td>9780877285151</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     );
 };
