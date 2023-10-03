7b5b24822af9e7fde3b1b2cd8c450c1adc43064d
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-03 13:08:31 -0600

    Creating a newLibraryForm component

--- src/components/homePage/newLibraryForm.js
+++ src/components/homePage/newLibraryForm.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 // CSS File
+import "./styles/newLibraryForm-styles.css";
 
 export default function NewLibraryForm() {
     return (
-        <div>This is the form for making a new library.</div>
+        <div>
+            {/* Button to show the form */}
+            <div className="row">
+                <div className="col-xs-12">
+                    <button>Add New Library</button>
+                </div>
+            </div>
+        </div>
     );
 };
