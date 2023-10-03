a3ec2cafc243a17ea71641600ccdfa3e4aa11155
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-03 10:42:23 -0600

    Creating the bookTable component

---
+++ src/components/homePage/bookTable.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// CSS File
+import "./styles/bookTable-styles.css";
+
+export default function BookTable() {
+    return (
+        <div>The book table goes here.</div>
+    );
+};
