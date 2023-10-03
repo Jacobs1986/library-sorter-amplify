85b8a78e4a0da25e706d232c0d69af55519d62c5
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-03 11:49:12 -0600

    Mapped out the booklist

--- src/components/homePage/bookTable.js
+++ src/components/homePage/bookTable.js
@@ -1,28 +1,44 @@
-import React from "react";
+import React, {
+    useContext
+} from "react";
 
 // CSS File
 import "./styles/bookTable-styles.css";
 
+// Import context
+import { BookList } from "./table";
+
 export default function BookTable() {
+    // library
+    const { library } = useContext(BookList);
+
     return (
         <div className="row">
-            {/* The table */}
-            <div className="col-xs-12" style={{ overflowX: "auto" }}>
-                <table className="bookTable">
-                    {/* Table header */}
-                    <tr>
-                        <th>Title</th>
-                        <th>Author</th>
-                        <th>ISBN</th>
-                    </tr>
-                    {/* Table row */}
-                    <tr>
-                        <td>Jung and Tarot: An Archetypal Journey</td>
-                        <td>Sallie Nichols</td>
-                        <td>9780877285151</td>
-                    </tr>
-                </table>
-            </div>
+            {/* Check the length of the bookList array */}
+            {library.bookList.length === 0 ?
+                <div className="col-xs-12 col-s-12 col-lg-3 col-xl-2">
+                    Time to add some books!
+                </div> :
+                // The table
+                <div className="col-xs-12" style={{ overflowX: "auto" }}>
+                    <table className="bookTable">
+                        {/* Table header */}
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>ISBN</th>
+                        </tr>
+                        {/* Table row */}
+                        {library.bookList.map((book, i) => (
+                            <tr key={i}>
+                                <td>{book.title}</td>
+                                <td>{book.author}</td>
+                                <td>{book.isbn}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+            }
         </div>
     );
 };
