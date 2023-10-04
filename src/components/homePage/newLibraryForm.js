0493df9db4a03ede97cfdfb9591ec0fb98bf9e2c
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-04 10:45:12 -0600

    Created a function for showing the form; right now just tested out adding a className to the span

--- src/components/homePage/newLibraryForm.js
+++ src/components/homePage/newLibraryForm.js
@@ -1,15 +1,40 @@
 import React from "react";
+import { AiFillCaretRight } from "react-icons/ai";
 
 // CSS File
 import "./styles/newLibraryForm-styles.css";
 
 export default function NewLibraryForm() {
+
+    // Funtion to show the form
+    const handleShowForm = event => {
+        // Get the elements id
+        let elementId = event.target.id;
+        // Get the next className of the span
+        let currentClass = document.getElementById(elementId).firstElementChild;
+        // Begin the switch statement
+        switch (currentClass.className) {
+            // If the currentClass is rightArrow
+            case "rightArrow":
+                // Add activeForm
+                currentClass.className += " activeForm";
+                break
+            // Default
+            default:
+                // Remove activeForm
+                currentClass.className = "rightArrow";
+        }
+    }
+
     return (
         <div className="md-font-size lg-font-size xl-font-size">
             {/* Button to show the form */}
             <div className="row">
                 <div className="col-xs-12">
-                    <div className="button">Add New Library</div>
+                    <div className="button" onClick={handleShowForm} id="showNewLibForm">
+                        Add New Library
+                        <span className="rightArrow" ><AiFillCaretRight /></span>
+                    </div>
                 </div>
             </div>
             {/* The form */}
