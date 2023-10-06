993e0e8b6bfb10d0f65620551149ec4a51b7c957
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-06 13:01:41 -0600

    Created banner

--- src/components/searchPage/banner/banner.js
+++ src/components/searchPage/banner/banner.js
@@ -5,6 +5,18 @@ import "./banner-styles.css";
 
 export default function Banner() {
     return (
-        <div>This is where the banner will go</div>
+        <div>
+            <div className="bannerContainer">
+                {/* Image */}
+                <img
+                    src={require("./magnifying-glass-with-book-table.jpg")}
+                    alt="Image of a book with a magnifying glass. Image by awesomecontent on Freepik"
+                    width={"100%"}
+                />
+                <div className="bottom-right attributionLink">
+                Image by <a href="https://www.freepik.com/free-photo/magnifying-glass-with-book-table_991460.htm#query=book%20search&position=5&from_view=search&track=ais" target="_blank">awesomecontent</a> on Freepik
+                </div>
+            </div>
+        </div>
     );
 };
