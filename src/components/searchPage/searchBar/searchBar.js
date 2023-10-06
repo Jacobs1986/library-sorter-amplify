ae68833136d206174bee3b51c7281f51398354e3
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-06 16:07:35 -0600

    Redid the layout for the search page. I like it better

--- src/components/searchPage/searchBar/searchBar.js
+++ src/components/searchPage/searchBar/searchBar.js
@@ -6,26 +6,42 @@ import "./searchBar-styles.css";
 export default function SearchBar() {
     return (
         <div>
-            {/* Search form */}
             <div className="row">
-                {/* Search input */}
-                <div className="col-xs-6 col-s-6 col-lg-5">
-                    <label className="boldLabel" html="searchParams">Search For:</label>
-                    <input type="text" id="searchParams" name="searchParams" />
+                {/* Picture */}
+                <div className="col-xs-12 col-s-6 imgCol">
+                    <div className="bannerContainer">
+                        <img
+                            src={require("./books-magnifying-glass.jpg")}
+                            width={"100%"}
+                        />
+                        {/* Attribution Link */}
+                        <div className="bottom-right attributionLink">
+                            Image by <a href="https://www.freepik.com/free-photo/books-magnifying-glass_22207266.htm#query=book%20search&position=3&from_view=search&track=ais" target="_blank">fabrikasimf</a> on Freepik
+                        </div>
+                    </div>
                 </div>
-                {/* Search select */}
-                <div className="col-xs-6 col-s-4 col-lg-3 col-xl-2">
-                    <label className="boldLabel" html="searchSelect">Search by:</label>
-                    <select id="searchSelect" name="searchSelect">
-                        <option value="title">Title</option>
-                        <option value="author">Author</option>
-                        <option value="isbn">ISBN</option>
-                        <option value="subject">Subject</option>
-                    </select>
-                </div>
-                {/* Form buttons */}
-                <div className="col-xs-12 col-s-2">
-                    <div className="button" id="searchBtn">Search</div>
+                {/* Search form */}
+                <div className="col-xs-12 col-s-6">
+                    <div className="formCol">
+                        {/* Search Params */}
+                        <div className="formPart">
+                            <label className="boldLabel" htmlFor="searchParams">Search for:</label>
+                            <input type="text" id="searchParams" name="searchParams" />
+                        </div>
+                        {/* Search Select */}
+                        <div className="formPart">
+                            <label className="boldLabel" html="searchSelect">Search by:</label>
+                            <select id="searchSelect" name="searchSelect">
+                                <option value="title">Title</option>
+                                <option value="author">Author</option>
+                                <option value="isbn">ISBN</option>
+                                <option value="subject">Subject</option>
+                            </select>
+                        </div>
+                        <div className="formPart">
+                            <div className="button" id="searchBtn">Search</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/book-search-page.js
+++ src/pages/book-search-page.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 // Components
-import Banner from "../components/searchPage/banner/banner";
 import SearchBar from "../components/searchPage/searchBar/searchBar";
 import SearchResults from "../components/searchPage/searchResults/searchResults";
 
 export default function BookSearch() {
     return (
         <div>
-            <Banner />
             <SearchBar />
             <SearchResults />
         </div>
