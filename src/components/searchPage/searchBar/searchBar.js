bac25caded2d338bacff0d1ef738a29192d6c3f2
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-06 14:12:11 -0600

    Added styling for the search form

--- src/components/searchPage/searchBar/searchBar.js
+++ src/components/searchPage/searchBar/searchBar.js
@@ -5,6 +5,29 @@ import "./searchBar-styles.css";
 
 export default function SearchBar() {
     return (
-        <div>This is where the search bar goes</div>
+        <div>
+            {/* Search form */}
+            <div className="row">
+                {/* Search input */}
+                <div className="col-xs-6 col-s-6 col-lg-5">
+                    <label className="boldLabel" html="searchParams">Search For:</label>
+                    <input type="text" id="searchParams" name="searchParams" />
+                </div>
+                {/* Search select */}
+                <div className="col-xs-6 col-s-4 col-lg-3 col-xl-2">
+                    <label className="boldLabel" html="searchSelect">Search by:</label>
+                    <select id="searchSelect" name="searchSelect">
+                        <option value="title">Title</option>
+                        <option value="author">Author</option>
+                        <option value="isbn">ISBN</option>
+                        <option value="subject">Subject</option>
+                    </select>
+                </div>
+                {/* Form buttons */}
+                <div className="col-xs-12 col-s-2">
+                    <div className="button" id="searchBtn">Search</div>
+                </div>
+            </div>
+        </div>
     );
 };
