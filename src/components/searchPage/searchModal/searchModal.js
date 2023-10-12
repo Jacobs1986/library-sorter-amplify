4d6147cd0b8a4ad031bf5257ebec0f0da808bae1
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-12 10:10:06 -0600

    Created a new basic modal

--- src/components/searchPage/searchModal/searchModal.js
+++ src/components/searchPage/searchModal/searchModal.js
@@ -2,9 +2,27 @@ import React from "react";
 
 // CSS File
 import "./searchModal-styles.css";
+import "../../../styles/modal.css";
 
 export default function SearchModal() {
     return (
-        <div>The search modal goes here.</div>
+        <div className="modal">
+            <div className="modal-content">
+                {/* Modal Header */}
+                <div class="modal-header">
+                    <span className="close">&times;</span>
+                    <h2>Modal Header</h2>
+                </div>
+                {/* Modal Body */}
+                <div className="modal-body">
+                    <p>Some text in the Modal Body</p>
+                    <p>Some other text...</p>
+                </div>
+                {/* Modal Footer */}
+                <div className="modal-footer">
+                    <h3>Modal Footer</h3>
+                </div>
+            </div>
+        </div>
     );
 };
