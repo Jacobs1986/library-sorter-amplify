9305d87e83d5cb478ef9a7277035de8f60c17cd9
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-13 12:35:08 -0600

    Redid the layout

--- src/components/searchPage/searchModal/basicInfo.js
+++ src/components/searchPage/searchModal/basicInfo.js
@@ -27,34 +27,51 @@ export default function BasicInfo() {
                 </div>
                 <div className="col-xs-12 col-s-8 col-m-7 col-lg-8 col-xl-9">
                     {/* Author list */}
-                    <div>
-                        <span className="modal-label">Author(s):</span>
-                        <span className="book-info">
+                    <div className="row">
+                        <div className="col-xs-12 col-s-4 col-m-4 col-lg-4 col-xl-3 modal-label">
+                            Author(s):
+                        </div>
+                        <div className="col-xs-12 col-s-8 col-m-8 col-lg-8 col-xl-9">
                             {googleInfo.authors.map((author, i) => (<span key={i}>{author} </span>))}
-                        </span>
+                        </div>
                     </div>
                     {/* ISBN */}
-                    <div className="modal-row">
-                        <span className="modal-label">ISBN:</span>
-                        <span className="book-info">{!googleISBN ? "NA" : googleISBN.identifier}</span>
+                    <div className="row">
+                        <div className="col-xs-12 col-s-4 col-m-4 col-lg-4 col-xl-3 modal-label">
+                            ISBN:
+                        </div>
+                        <div className="col-xs-12 col-s-8 col-m-8 col-lg-8 col-xl-9">
+                            {!googleISBN ? "NA" : googleISBN.identifier}
+                        </div>
                     </div>
                     {/* Publsiher */}
-                    <div className="modal-row">
-                        <span className="modal-label">Publisher:</span>
-                        <span className="book-info">{googleInfo.publisher}</span>
+                    <div className="row">
+                        <div className="col-xs-12 col-s-4 col-m-4 col-lg-4 col-xl-3 modal-label">
+                            Publisher:
+                        </div>
+                        <div className="col-xs-12 col-s-8 col-m-8 col-lg-8 col-xl-9">
+                            {googleInfo.publisher}
+                        </div>
                     </div>
                     {/* Publication Date */}
-                    <div className="modal-row">
-                        <span className="modal-label">Publication Date:</span>
-                        <span className="book-info">{googleInfo.publishedDate}</span>
+                    <div className="row">
+                        <div className="col-xs-12 col-s-4 col-m-4 col-lg-4 col-xl-3 modal-label">
+                            Publication Date:
+                        </div>
+                        <div className="col-xs-12 col-s-8 col-m-8 col-lg-8 col-xl-9">
+                            {googleInfo.publishedDate}
+                        </div>
                     </div>
                     {/* Description */}
-                    <div className="modal-row">
-                        <div className="modal-label">Description:</div>
-                        <div className="book-info">
+                    <div className="row">
+                        <div className="col-xs-12 modal-label">
+                            Description:
+                        </div>
+                        <div className="col-xs-12">
                             {!googleInfo.description ? "NA" : parse(googleInfo.description)}
                         </div>
                     </div>
+                    {/* Add library select */}
                 </div>
             </div>
         </>
