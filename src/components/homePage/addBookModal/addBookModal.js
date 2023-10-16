4f149cc69345da11a5a571ea14871bfa394d48b4
Sean Jacobson <sean.jacobson86@gmail.com>
2023-10-16 11:56:14 -0600

    Created library select

--- src/components/homePage/addBookModal/addBookModal.js
+++ src/components/homePage/addBookModal/addBookModal.js
@@ -1,16 +1,24 @@
 import React, {
+    useContext,
     useState
 } from "react";
 
 // CSS File
 import "./addBookModal-styles.css";
 
+// Import contexts
+import { Libraries } from "../../../App";
+
 export default function AddBookModal() {
+    // Libraries value
+    const { libraries } = useContext(Libraries);
     // showModal value
     const [showModal, setShowModal] = useState(false);
 
     // Function for hiding the modal
     const handleHideModal = () => {
+        // Libraries values
+        const { libraries } = useContext(Libraries);
         // toggle showModal
         setShowModal(!showModal);
     }
@@ -26,7 +34,35 @@ export default function AddBookModal() {
                 </div>
                 {/* Modal Body */}
                 <div className="modal-body">
-                    Modal body goes here.
+                    {/* Library select */}
+                    <div className="row">
+                        <div className="col-xs-12 col-s-4 col-m-3 col-lg-2 col-xl-2 modal-label">
+                            Add to Library:
+                        </div>
+                        <div className="col-xs-12 col-s-4 col-m-4 col-lg-3 col-xl-2">
+                            {!libraries ?
+                                <select className="modal-form">
+                                    <option value="">---</option>
+                                </select> :
+                                <select className="modal-form" name="libraryID">
+                                    <option value="">---</option>
+                                    {libraries.map(name => (
+                                        <option key={name.id} value={name.id}>{name.name}</option>
+                                    ))}
+                                </select>}
+                        </div>
+                    </div>
+                    {/* Forms */}
+                    <div className="row">
+                        <div className="col-xs-12 col-s-12 col-m-12 col-lg-12 col-xl-12">
+                            The basic information form goes here.
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-12 col-s-12 col-m-12 col-lg-12 col-xl-12">
+                            The collector information form goes here.
+                        </div>
+                    </div>
                 </div>
                 {/* Modal Footer */}
                 <div className="modal-footer">
